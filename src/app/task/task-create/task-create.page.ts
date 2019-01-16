fb794c03a5046df5dd6065e6544e45635a9c93f6
Pedro <pedrolopes.info@gmail.com>
2019-01-16 01:06:56 -0200

    cadastrar tarefa

--- src/app/task/task-create/task-create.page.ts
+++ src/app/task/task-create/task-create.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Regex } from '../../regex';
+import { ProjectService } from '../../project/project.service';
+import { TaskService } from '../task.service';
+import { Project } from '../../project/project.model';
+import { Category } from '../category';
+import { AlertService } from '../../shared/alert/alert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-create',
@@ -9,18 +16,52 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class TaskCreatePage implements OnInit {
 
   createTaskForm: FormGroup;
+  projects: Project;
+  categories: Category;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private regex: Regex,
+    private projectService: ProjectService,
+    private taskService: TaskService,
+    private alert: AlertService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.createTaskForm = this.formBuilder.group({
-      nome: this.formBuilder.control(['']),
-      categoria: this.formBuilder.control(['']),
-      projeto: this.formBuilder.control(['']),
-      inicio: this.formBuilder.control(['']),
-      entrega: this.formBuilder.control(['']),
-      descricao: this.formBuilder.control(['']),
+      nome: this.formBuilder.control('', [Validators.required, Validators.pattern(this.regex.alphabeticRegex)]),
+      categoria: this.formBuilder.control('', [Validators.required]),
+      projeto: this.formBuilder.control('', [Validators.required]),
+      inicio: this.formBuilder.control('', [Validators.required]),
+      entrega: this.formBuilder.control('', [Validators.required]),
+      descricao: this.formBuilder.control('', [Validators.required]),
     });
+  }
+
+  ionViewWillEnter() {
+    this.projectService.getProjects()
+      .subscribe(response => {
+        this.projects = response;
+      });
+
+    this.taskService.getCategories()
+      .subscribe(response => {
+        this.categories = response;
+      });
+  }
+
+  onSubmit() {
+    this.taskService.createTask(this.createTaskForm.value)
+      .subscribe(response => {
+        this.alert.header = response.success;
+        this.alert.message = response.message;
+        this.alert.buttons = [{
+                                text: 'OK',
+                                handler: () => this.router.navigate([`/project/${this.createTaskForm.get('projeto').value}`]) 
+                              }];
+        this.alert.presentAlert();
+      });
   }
 
 }
